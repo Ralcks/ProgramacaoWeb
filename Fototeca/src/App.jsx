2215608621932770dbbdf06d4d5bd8589b4b1443
Ramias <ramiaslopes@gmail.com>
2023-11-30 21:26:43 -0300

    Fototeca
    Projeto inicializado.

---
+++ Fototeca/src/App.jsx
@@ -0,0 +1,13 @@
+// App.jsx
+import React from 'react';
+import Inicio from './components/Inicio';
+
+function App() {
+  return (
+    <div>
+      <Inicio />
+    </div>
+  );
+}
+
+export default App;
---
+++ Fototeca/src/components/Inicio.jsx
@@ -0,0 +1,13 @@
+// Inicio.jsx
+import React from 'react';
+
+const Inicio = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à Fototeca</h1>
+      <p>A sua plataforma para gerenciar fotos.</p>
+    </div>
+  );
+};
+
+export default Inicio;
