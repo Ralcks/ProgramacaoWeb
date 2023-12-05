2b9a8aaaebffea1b4c68e6b971b5507344277549
Ramias <ramiaslopes@gmail.com>
2023-12-04 23:22:59 -0300

    Tela inicio
    Sem rotas por enquanto

--- Fototeca/src/components/Inicio.jsx
+++ Fototeca/src/components/Inicio.jsx
@@ -1,14 +1,28 @@
 // src/components/Inicio.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import './Inicio.css'; // Você pode criar um arquivo CSS para estilizar o componente
 import Botao from './Botao';
 
 const Inicio = () => {
+  const [nome, setNome] = useState('');
+
+  const handleEntrarClick = () => {
+    // Lógica para redirecionar para próxima tela com o nome, se necessário
+    console.log('Nome:', nome);
+  };
+
   return (
     <div className='inicio'>
       <h1>Bem-vindo à Fototeca</h1>
       <p>A sua plataforma para gerenciar fotos.</p>
-      <Botao />
+      <input
+        type="text"
+        id="nome"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+        placeholder="Digite seu nome"
+      />
+      <Botao onClick={handleEntrarClick}>Entrar</Botao>
     </div>
   );
 };
